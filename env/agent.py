Fixing some bugs. (maneuver formula)

env/agent.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 import tensorflow_probability as tfp
 
 class Agent():
-    def __init__(self, acceptableDistance=9260):
+    def __init__(self, acceptableDist=9260):
         self.xPos = 0
         self.yPos = 0
         self.firstPosX = 0
@@ -19,15 +19,14 @@ class Agent():
         self.accel = {'ax': 0, 'ay': 0}
         self.firstSpeed = self.speed
         self.id = 0
-        self.acceptableDist = acceptableDistance
+        self.acceptableDist = acceptableDist
         self.reward = 0
         self.nonVectoralSpeedStart = 250
         self.nonVectoralSpeed = 250
         self.angle = 0
         self.nonVectoralAccel = 0
         self.timetoManouver = 160
-        
-        
+           
     def initModel(self, n_actions=4, learning_rate=0.0001, gamma=0.99):
         self.gamma = gamma
         self.n_actions = n_actions
@@ -80,7 +79,6 @@ class Agent():
             (grad, var) 
             for (grad, var) in zip(
             gradient, self.actor_critic.trainable_variables) if grad is not None])
-
 
     def initRandomPosition(self, xWidth, yWidth, agents, id):
         loactionEmpty = []
@@ -159,8 +157,7 @@ class Agent():
         """
         self.speed['vx'] = np.cos(Si) * u
         self.speed['vy'] = np.sin(Si) * u
-        self.angle = Si
-        
+        self.angle = Si 
 
     def initLine(self):
         self.slope = (self.yDest - self.yPos) / (self.xDest - self.xPos)
@@ -211,30 +208,34 @@ class Agent():
         Dist = np.sqrt((self.getAttr()['xPos'] - self.getAttr()['xDest']) ** 2 + (self.getAttr()['yPos'] - self.getAttr()['yDest']) ** 2)
         return Dist
 
-    def TTCD(self, agent): # Calculating time to collision for direct move.
+    def TTCD(self, target): # Calculating time to collision for direct move.
         ttc = Symbol('ttc')
         ttcValue = solve(((self.xPos + self.speed['vx'] * ttc) - \
-                        (agent.xPos + agent.speed['vx'] * ttc)) ** 2 + \
+                        (target.xPos + target.speed['vx'] * ttc)) ** 2 + \
                         ((self.yPos + self.speed['vy'] * ttc) - \
-                        (agent.yPos + agent.speed['vy'] * ttc)) ** 2 - 46300, ttc)
+                        (target.yPos + target.speed['vy'] * ttc)) ** 2 - 46300, ttc)
         return ttcValue
 
-    def TTCM(self, agent): # Calculating time to collision for maneuver move.
+    def TTCM(self, target): # Calculating time to collision for maneuver move.
         ttc = Symbol('ttc')
         ttcValue = solve(((self.xPos + self.speed['vx'] * ttc + 0.5 * self.accel['ax'] * (ttc ** 2)) - \
-                        (agent.xPos + agent.speed['vx'] * ttc + 0.5 * agent.accel['ax'] * (ttc ** 2))) ** 2 + \
-                        ((self.yPos + self.speed['vy'] * ttc + 0.5 * self.accel['va'] * (ttc ** 2)) - \
-                        (agent.yPos + agent.speed['vy'] * ttc + 0.5 * agent.accel['va'] * (ttc ** 2))) ** 2, ttc )
-        print("ttcValue for maneuver move: ", ttcValue) 
+                        (target.xPos + target.speed['vx'] * ttc + 0.5 * target.accel['ax'] * (ttc ** 2))) ** 2 + \
+                        ((self.yPos + self.speed['vy'] * ttc + 0.5 * self.accel['ay'] * (ttc ** 2)) - \
+                        (target.yPos + target.speed['vy'] * ttc + 0.5 * target.accel['ay'] * (ttc ** 2))) ** 2, ttc )
+        # print("ttcValue for maneuver move: ", ttcValue) 
 
-    def sensor(self, agents):
+    def sensor(self, agents, ismanouver):
         sensorAlarm=[]
         for target in agents:
             if self.id == target.id:
                 continue
             else:
-                Dist = self.distfromAgent(target)
-                ttc = self.TTCD(target)
+                if ismanouver:
+                    Dist = self.distfromAgent(target)
+                    ttc = self.TTCM(target)
+                else:
+                    Dist = self.distfromAgent(target)
+                    ttc = self.TTCD(target)
                 if not bool(ttc):
                     sensorAlarm.append(False)
                     break
@@ -266,8 +267,8 @@ class Agent():
 
     def maneuverMove(self, angle, nonVectoralSpeed, changedAngle, changedAccel, deltaT):
         self.updateAcceleration(angle, nonVectoralSpeed , changedAngle, changedAccel, deltaT)
-        self.xPos = self.xPos + self.speed['vx'] + 0.5 * self.accel['ax'] * (deltaT ** 2)
-        self.yPos = self.yPos + self.speed['vy'] + 0.5 * self.accel['ay'] * (deltaT ** 2)
+        self.xPos = self.xPos + self.speed['vx'] * deltaT + 0.5 * self.accel['ax'] * (deltaT ** 2)
+        self.yPos = self.yPos + self.speed['vy'] * deltaT + 0.5 * self.accel['ay'] * (deltaT ** 2)
 
     def angleFromPathLine(self):
         distance = [self.xDest - self.xPos, self.yDest - self.yPos]

env/evn.py
@@ -16,7 +16,6 @@ class Env():
     def initAgent(self, agnetNum=2, random=True):
         agentList = []
         if random:
-            # print("init agent random")
             for i in range(agnetNum):
                 print(agentList)
                 agn = Agent()
@@ -24,12 +23,14 @@ class Env():
                 agentList.append(agn)
             return agentList
         else:
-            # print("init agent predefine")
             ag = Agent()
             ag.initPredefinePosition(x=0, y=0, xD=self.xWidth, yD=self.yWidth, id=0)
             agentList.append(ag)
             ag = Agent()
             ag.initPredefinePosition(x=self.xWidth, y=self.yWidth, xD=0, yD=0, id=1)
+            agentList.append(ag)
+            ag = Agent()
+            ag.initPredefinePosition(x=0, y=0, xD=self.xWidth, yD=self.yWidth, id=2)
             agentList.append(ag)
             return agentList
 
@@ -40,7 +41,6 @@ class Env():
         else:
             changedAccel = action['accel'].numpy()
             changedAngle = action['angle'].numpy()
-            # print(f"\n@@@@@@@@\tInside step changedAccel {changedAccel}, changedAngle {changedAngle}")
             agent.maneuverMove(agent.angle, agent.nonVectoralSpeed, changedAngle, changedAccel, deltaT)
             return [agentList[0].xPos, agentList[0].yPos, agentList[0].speed['vx'], agentList[0].speed['vy'], agentList[0].accel['ax'], agentList[0].accel['ay'], agentList[1].xPos, agentList[1].yPos, agentList[1].speed['vx'], agentList[1].speed['vy'], agentList[1].accel['ax'], agentList[1].accel['ay']], self.stepReward(agent, agentList), None, None
         
@@ -113,23 +113,25 @@ class Env():
             return agent.reward
         
         # 2- Heading error and Cross Error reward
-        # if not agent.checkArrival(): # what is kc? what is kv? what is delta u p?
-        #     print("reward Heading") 
+        if not agent.checkArrival(): # what is kc? what is kv? what is delta u p?
+            # print("reward Heading") 
 
-        #     rHeadingCross1 = np.exp(-k_c * np.abs(agent.distfromPathLine())) * np.cos(agent.angleFromPathLine()) + k_r * (np.exp(-k_c * np.abs(agent.distfromPathLine())) + np.cos(agent.angleFromPathLine())) + np.exp(-k_v * np.abs(deltaUp)) - R_c
-        #     rHeadingCross2 = np.exp(-k_c * np.abs(agent.distfromPathLine())) + np.exp(-k_c * np.abs(agent.distfromPathLine())) + np.exp(-k_v * np.abs(deltaUp)) - R_c
-        #    # agent.reward += (rHeadingCross1 + rHeadingCross2)/2
-        #     agent.reward += rHeadingCross1
-        #     print(f"rHeadingCross1: {rHeadingCross1}")
+            rHeadingCross1 = np.exp(-k_c * np.abs(agent.distfromPathLine())) * np.cos(agent.angleFromPathLine()) + k_r * (np.exp(-k_c * np.abs(agent.distfromPathLine())) + np.cos(agent.angleFromPathLine())) + np.exp(-k_v * np.abs(deltaUp)) - R_c
+            rHeadingCross2 = np.exp(-k_c * np.abs(agent.distfromPathLine())) + np.exp(-k_c * np.abs(agent.distfromPathLine())) + np.exp(-k_v * np.abs(deltaUp)) - R_c
+           # agent.reward += (rHeadingCross1 + rHeadingCross2)/2
+            agent.reward += rHeadingCross1
+            # print(f"rHeadingCross1: {rHeadingCross1}")
         
         # B) Collision Avoidance Reward function
-        if agent.distfromAgent(target) < 9260:
-            print("reward dist from agent is low", agent.distfromAgent(target))
+        if agent.distfromAgent(target) < agent.acceptableDist:
+            # print("reward dist from agent is low", agent.distfromAgent(target))
             agent.reward += rewardCollision
             return agent.reward
         if agent.checkLeftofLine() > 1e-06:
             print("reward going left of line")
             agent.reward += rewardLeft
+            return agent.reward
+
         return agent.reward
         
 